604cf2487b7c9196a02bc2f9aac3b9a3e2bcb503
Pedro <pedromello172@hotmail.com>
2022-09-28 22:47:18 -0300

    criando alguns componetes e arquivos para o quiz

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Welcome from './components/welcome'
 
 function App() {
 
   return (
     <div className="App">
-     <h1>Quiz App</h1>
+     <h1>Quiz de Programação</h1>
+     <Welcome />
     </div>
   )
 }
