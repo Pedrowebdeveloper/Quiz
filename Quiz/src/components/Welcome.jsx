8330e7ffa1bb473516b4eb7d5feb9aaef117da54
Pedro <pedromello172@hotmail.com>
2022-09-29 20:37:38 -0300

    Fazendo as ações que m udan o estadod a aplicação

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -1,22 +1,23 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
+import "./Welcome.css";
+
 import Quiz from "../img/quiz.svg";
 
-import "./welcome.css";
-
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
-  console.log(quizState)
   return (
     <div id="welcome">
-        <h2>Seja bem-vindo</h2>
-        <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Inicio do quiz" />
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
