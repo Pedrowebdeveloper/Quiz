97f5dddfa35594aa6c62c18a27b05ae60a8d904d
Pedro <pedromello172@hotmail.com>
2022-09-29 20:57:12 -0300

    Fazendo as ações que mudam os estagios da aplicação

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,15 +1,22 @@
 //react, componentes, estáticos
+import { useContext } from "react";
+import { QuizContext } from "./context/quiz";
+
 import Welcome from "./components/welcome";
+import question from "./components/Question";
 
 import './App.css'
+import Question from "./components/Question";
 
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
      <h1>Quiz de Programação</h1>
-     <Welcome />
+      {quizState.gameStage == "Start" && <Welcome />}
+      {quizState.gameStage == "Playing" && <Question />}
     </div>
   )
 }
---
+++ Quiz/src/components/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Question = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+
+  return <div>Question</div>
+}
+
+export default Question
